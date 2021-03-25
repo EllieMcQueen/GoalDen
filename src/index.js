b28492c1af84954d8e9301ac03a7a9c0d2ac2fcf
EllieMcQueen <70542907+EllieMcQueen@users.noreply.github.com>
2021-03-25 14:37:13 -0600

    env

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter, BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Router = 
+  process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
